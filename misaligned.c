1274f45c743ee5bfdbbda7b68c1abc0be1de1e2a
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-03-29 16:34:07 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -5,33 +5,35 @@
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+
 struct Colorpair
 {
-	int Number;
+	int Num;
 	char MajorColor[8];
 	char MinorColor[8];
 };
 
 Colorpair colorpair[25];
 
-void printOnConsole(int ColorPairArrayIndex, char *majorColor, char *minorColor)
+void PrintOnConsole(int ColorPairArrayIndex, char *majorColor, char *minorColor)
 {
 printf("%d | %s | %s\n", ColorPairArrayIndex, majorColor, minorColor);
 }
 
 int printColorMap() {
     
-    int i = 0, j = 0,ColorPairArrayIndex = 1;
+    int i = 0, j = 0, ColorPairIdx=1;
     for(i = 0; i < 5; i++) 
 	{
         for(j = 0; j < 5; j++)
 		{
 			
-			colorpair[ColorPairArrayIndex].Number = ColorPairArrayIndex;
-			strcpy(colorpair[ColorPairArrayIndex].MajorColor , majorColor[i]);
-			strcpy(colorpair[ColorPairArrayIndex].MinorColor , minorColor[j]);
-			printOnConsole(ColorPairArrayIndex,colorpair[ColorPairArrayIndex].MajorColor,colorpair[ColorPairArrayIndex].MinorColor);	
-		        ColorPairArrayIndex++;				          
+			colorpair[ColorPairIdx].Num = ColorPairIdx;
+			strcpy(colorpair[ColorPairIdx].MajorColor , majorColor[i]);
+			strcpy(colorpair[ColorPairIdx].MinorColor , minorColor[j]);
+			PrintOnConsole(ColorPairIdx, colorpair[ColorPairIdx].MajorColor, colorpair[ColorPairIdx].MinorColor);	
+		    ColorPairIdx++;
+				          
         }
     }
     return i * j;
@@ -40,12 +42,13 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-	assert(colorpair[23].Number == 23);	
-	assert(strcmp(colorpair[23].MajorColor, "Violet") == 0);
-	assert(strcmp(colorpair[23].MinorColor, "Green") == 0);
-	assert(colorpair[15].Number == 15);	
+	assert(colorpair[6].Num == 6);	
+	assert(strcmp(colorpair[6].MajorColor, "Red") == 0);
+	assert(strcmp(colorpair[6].MinorColor, "Blue") == 0);
+	assert(colorpair[15].Num == 15);	
 	assert(strcmp(colorpair[15].MajorColor, "Black") == 0);
 	assert(strcmp(colorpair[15].MinorColor, "Slate") == 0);
+
     printf("All is well (Yes,it is! :> )\n");
     return 0;
 }
